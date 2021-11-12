ed98b9f17e13f1c32288478055ff7e6357667f2a
JolaWalach <joanna.walach@op.pl>
2021-11-12 01:57:28 +0100

    axios added

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import Book from './Book';
+import {useState, useEffect } from 'react';
+import axios from 'axios';
+import book from './Book';
 
-function BookList() {
+const BookList = () => {
+    const [books, setBooks] = useState([]);
+        
+    useEffect(()=>{
+        
+        axios.get("https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=AIzaSyCNGtCug7Ln0l5_4BDDeJBY97kHN0WuoyU")
+        .then(response => setBooks(response.data.items))
+        .catch(err => console.log(err))
+      },[]);
     return (
-        <div>
-            <Book />
+        <div className="container">
+            {books.map(book => (
+            <div className="card" >
+              <p key={book.id}>{book.volumeInfo.subtitle}</p>
+              </div>
+            ))}
         </div>
-    )
-}
+      )
+    };
 
-export default BookList
+export default BookList;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,58 @@
-import styled from 'styled-components';
 import React from 'react';
+import styled from 'styled-components';
 import HeaderImg from './../img/header.jpg';
 
 const MainDiv = styled.div `
-margin-left: auto;
-margin-right: auto;
+
 `
+
+const Heading = styled.h2`
+position = absolute;
+`
+
 const Image = styled.img`
 width: 100%;
 height: 50vh;
 `
-const Input = styled.input`
-width: 100vh;
-height: 5vh;
+
+const InputDiv = styled.div`
 position: absolute;
-top: 30vh;
-left: 5vh;
-z-Index: 1;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+width: 100%;
+height: 50vh;
+`
+const Input = styled.input `
+width: 60%;
+height: 2rem;
+border:0;
+outline: 0;
+`
+
+const Button = styled.button`
+background-color: red;
+color: white;
+border: 0;
+outline: 0;
+height: 2.1rem;
 `
 
 function Header() {
     return (
         <MainDiv>
+            <Heading>Library App</Heading>
+            <InputDiv>
             <Input />
-            <Image src={HeaderImg} alt="header"/>
-            
+            <Button>Text</Button>
+            </InputDiv>
+            <Image src={HeaderImg} alt="header" />
         </MainDiv>
+
+ 
+            
+
     )
 }
 
